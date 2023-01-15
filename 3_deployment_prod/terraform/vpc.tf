4dc1ea0a7a31039be4a9dffe59897b4a1e236e35
Nicola Benaglia <nico.benaz@gmail.com>
2023-01-15 18:22:47 +0100

    Add elastic ip

--- 3_deployment_prod/terraform/vpc.tf
+++ 3_deployment_prod/terraform/vpc.tf
@@ -57,8 +57,13 @@ resource "aws_internet_gateway" "ig" {
   }
 }
 
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
 resource "aws_nat_gateway" "nat" {
   connectivity_type = "public"
+  allocation_id     = aws_eip.nat.id
   subnet_id         = aws_subnet.private_subnet.id
   depends_on        = [aws_internet_gateway.ig]
 }
